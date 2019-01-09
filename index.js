85c35ae3734a8b2d751504733e995404c6205da9
AdilsonAngelo <aasj2@cin.ufpe.br>
2019-01-09 17:29:20 -0300

    Table implementation

--- index.js
+++ index.js
@@ -1,11 +1,72 @@
 console.log($('#result'));
 
-$.ajax('https://fundamentusapi.herokuapp.com/',{
-    type: 'GET',
-    async: true,
-    xhrFields: { withCredentials: true },
+var stocks;
+
+// AVKEY: VS1Q0W7ZFJHF53Z4
+
+$.ajax('https://fundamentusapi.herokuapp.com/', {
+	type: 'GET',
+	async: true,
 })
-    .then(res => {
-        console.log(res);
-    })
-    .catch(err => console.error(err));
+	.then(res => {
+		stocks = [];
+
+		for (let s in res) {
+			if (res.hasOwnProperty(s)) {
+				let stock = res[s];
+				stock.tick = s;
+				stocks.push(stock);
+			}
+		}
+
+		console.log(stocks);
+
+
+		for (let s of stocks) {
+			for (let key in s) {
+				if (s.hasOwnProperty(key)) {
+					if (key != 'tick') s[key] = parseFloat(s[key].replace('.', '').replace(',', '.'));
+				}
+			}
+		}
+
+		stocks.sort((a, b) => a.ROIC < b.ROIC ? -1 : (a.ROIC > b.ROIC ? 1 : 0));
+
+		let i = 0;
+		for (let s of stocks) {
+			s.greenblatt = i;
+			i++;
+		}
+
+		stocks.sort((a, b) => a['EV/EBIT'] > b['EV/EBIT'] ? -1 : (a['EV/EBIT'] < b['EV/EBIT'] ? 1 : 0));
+
+		i = 0;
+		for (let s of stocks) {
+			s.greenblatt += i;
+			i++;
+		}
+
+		stocks.sort((a, b) => a.greenblatt > b.greenblatt ? -1 : (a.greenblatt < b.greenblatt ? 1 : 0));
+
+		i = 0
+		for (let s of stocks) {
+			$('#result tbody').append(
+				"<tr>" +
+				"<td>" + i + "</td>" +
+				"<td>" + s.tick + "</td>" +
+				"<td>" + s.cotacao + "</td>" +
+				"<td>" + s['P/L'] + "</td>" +
+				"<td>" + s.DY + "</td>" +
+				"<td>" + s.ROIC + "% </td>" +
+				"<td>" + s.ROE + "% </td>" +
+				"<td>" + s['Pat.Liq'] + "</td>" +
+				"<td>" + s['EV/EBIT'] + "% </td>" +
+				"<td>" + s.greenblatt + "</td>" +
+				"</tr>"
+			);
+			i++;
+		}
+
+		$('table').DataTable();
+	})
+	.catch(err => console.error(err));
