78c0f8c6beb670fcae8d3472b8e4569ec0792253
AdilsonAngelo <aasj2@cin.ufpe.br>
2019-01-11 01:44:45 -0300

    Massive improvement that cannot fit in a commit message

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-console.log($('#result'));
+// AVKEY: VS1Q0W7ZFJHF53Z4
+String.prototype.replaceAll = function (search, replacement) {
+	return this.split(search).join(replacement);
+}
 
 var stocks;
-
-// AVKEY: VS1Q0W7ZFJHF53Z4
 
 $.ajax('https://fundamentusapi.herokuapp.com/', {
 	type: 'GET',
@@ -19,54 +20,61 @@ $.ajax('https://fundamentusapi.herokuapp.com/', {
 			}
 		}
 
-		console.log(stocks);
-
-
 		for (let s of stocks) {
 			for (let key in s) {
 				if (s.hasOwnProperty(key)) {
-					if (key != 'tick') s[key] = parseFloat(s[key].replace('.', '').replace(',', '.'));
+					if (key != 'tick') s[key] = parseFloat(s[key].replaceAll('.', '').replace(',', '.'));
 				}
 			}
 		}
 
-		stocks.sort((a, b) => a.ROIC < b.ROIC ? -1 : (a.ROIC > b.ROIC ? 1 : 0));
+		stocks = stocks.filter(s => s['Liq.2m.'] > 1e8);
+		stocks.sort((a, b) => a.ROIC > b.ROIC ? -1 : (a.ROIC < b.ROIC ? 1 : 0));
 
-		let i = 0;
+		let i = 1;
 		for (let s of stocks) {
 			s.greenblatt = i;
 			i++;
 		}
 
-		stocks.sort((a, b) => a['EV/EBIT'] > b['EV/EBIT'] ? -1 : (a['EV/EBIT'] < b['EV/EBIT'] ? 1 : 0));
+		stocks.sort((a, b) => a['EV/EBIT'] < b['EV/EBIT'] ? -1 : (a['EV/EBIT'] > b['EV/EBIT'] ? 1 : 0));
 
-		i = 0;
+		i = 1;
 		for (let s of stocks) {
 			s.greenblatt += i;
 			i++;
 		}
 
-		stocks.sort((a, b) => a.greenblatt > b.greenblatt ? -1 : (a.greenblatt < b.greenblatt ? 1 : 0));
+		stocks.sort((a, b) => a.greenblatt < b.greenblatt ? -1 : (a.greenblatt > b.greenblatt ? 1 : 0));
 
-		i = 0
+		i = 1
 		for (let s of stocks) {
 			$('#result tbody').append(
 				"<tr>" +
 				"<td>" + i + "</td>" +
 				"<td>" + s.tick + "</td>" +
-				"<td>" + s.cotacao + "</td>" +
+				"<td>R$ " + real.format(s.cotacao) + "</td>" +
 				"<td>" + s['P/L'] + "</td>" +
-				"<td>" + s.DY + "</td>" +
+				"<td>" + s.DY + "% </td>" +
 				"<td>" + s.ROIC + "% </td>" +
 				"<td>" + s.ROE + "% </td>" +
-				"<td>" + s['Pat.Liq'] + "</td>" +
-				"<td>" + s['EV/EBIT'] + "% </td>" +
+				"<td>" + real.format(s['Pat.Liq']) + "</td>" +
+				"<td>" + s['EV/EBIT'] + "</td>" +
+				"<td>" + real.format(s['Liq.2m.']) + "</td>" +
 				"<td>" + s.greenblatt + "</td>" +
 				"</tr>"
 			);
 			i++;
 		}
 
-		$('table').DataTable();
+		$('table').DataTable({
+			responsive: true
+		});
 	})
 	.catch(err => console.error(err));
+
+var real = new Intl.NumberFormat('pt-BR', {
+	// style: 'currency',
+	currency: 'BRL',
+	minimumFractionDigits: 2,
+});
